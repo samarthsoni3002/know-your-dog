4628442cd33a8cd33d3f75a5c83269acc422b7c9
samarth <samarthsoni3002@gmail.com>
2025-06-10 15:17:28 +0530

    chore: enable CORS for frontend-backend communication

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI 
 from app.api.routes import router 
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
@@ -8,6 +9,13 @@ app = FastAPI(
     version = "1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
